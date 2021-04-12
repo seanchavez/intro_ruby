104c707b832d356d931c61ec9146e149caed9b68
seanchavez <realseanchavez@gmail.com>
2021-04-12 11:26:28 -0700

    exercise 3

--- variables/age.rb
+++ variables/age.rb
@@ -8,3 +8,4 @@ puts "In 30 years you will be:"
 puts age + 30
 puts "In 40 years you will be"
 puts age + 40
+
--- variables/name.rb
+++ variables/name.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Whatup #{name}!"
+
+10.times do
+  puts name
+end
