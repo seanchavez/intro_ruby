fc6744676270e0bce47db6683df9d86380428904
seanchavez <realseanchavez@gmail.com>
2021-04-17 11:06:05 -0700

    exercises 13, 14

--- exercises/exercises.rb
+++ exercises/exercises.rb
@@ -38,3 +38,32 @@ hash_3 = {array: arr}
 p hash_3
 hashes = [hash_1, hash_2, hash_3]
 p hashes
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# 11. 
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1] 
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+p contacts
+
+# 12.
+puts contacts["Joe Smith"][:email]
+puts contacts["Sally Johnson"][:phone]
+
+# 13.
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if {|s| s.start_with?('s', 'w')}
+p arr
+  
+# 14.
+a = ['white snow', 'winter wonderland', 'melting ice',
+  'slippery sidewalk', 'salted roads', 'white trees']
+
+ p a.map {|s| s.split}.flatten
