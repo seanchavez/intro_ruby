b27ae43d84e5afd35b4fe60861c73745f97e77df
seanchavez <realseanchavez@gmail.com>
2021-04-18 23:59:42 -0700

    loops 1

--- ruby_basics/loops.rb
+++ ruby_basics/loops.rb
@@ -18,10 +18,51 @@ end
 puts 'This is outside all loops.'
 
 # control the loop
-iterations = 1
+iterations = 5
 
 loop do
   puts "Number of iterations = #{iterations}"
-  iterations += 1
-  break if iterations > 5
+  break if iterations <= 1
+  iterations -= 1
+end
+
+#loop on command
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+end
+
+# say hello
+say_hello = true
+count = 0
+
+while say_hello
+  puts 'Hello!'
+  count += 1
+  say_hello = false if count == 5
+end
+
+# print while
+ numbers = []
+
+while numbers.length < 5
+  numbers << rand(100)
+end
+puts numbers
+
+# count up
+count = 1
+
+until count > 10
+  puts count
+  count += 1
+end
+
+# print until
+numbers = [7, 9, 13, 25, 18]
+count = 0
+until count == numbers.length
+  puts numbers[count]
+  count += 1
 end
