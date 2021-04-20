214613793cc1fef9a486954586c03a081077761f
seanchavez <realseanchavez@gmail.com>
2021-04-19 17:48:53 -0700

    complete loops 2

--- ruby_basics/loops_2.rb
+++ ruby_basics/loops_2.rb
@@ -47,3 +47,48 @@ loop do
   break if numbers.length == 5
 end
 puts numbers
+
+# empty the array
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+loop do
+  puts names.pop
+  break if names.empty?
+end
+
+# stop counting
+5.times do |index|
+  puts index
+  break if index == 2
+end
+
+# only even
+number = 0
+
+until number == 10
+  number += 1
+  next if number.odd?
+  puts number
+end
+
+# first to five
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+  next unless number_a == 5 || number_b == 5
+  puts "5 was reached!"
+  break
+end
+
+# greetings
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+while number_of_greetings > 0
+  greeting
+  number_of_greetings -= 1
+end
