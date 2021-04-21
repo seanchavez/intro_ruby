5c5ea3f5124d118161ca561e6c85f5cd827356be
seanchavez <realseanchavez@gmail.com>
2021-04-21 11:20:54 -0700

    debug, cleanup user_input/division.rb

--- ruby_basics/user_input/division.rb
+++ ruby_basics/user_input/division.rb
@@ -2,24 +2,23 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string
 end
 
-dividend = divisor = nil
+dividend = nil
 loop do
   puts "Please enter the dividend:"
   dividend = gets.chomp
   break if valid_number?(dividend)
-  puts "Integers only."
+  puts "Invalid input. Integers only."
 end
+divisor = nil
 loop do
   puts "Please enter the divisor:"
   divisor = gets.chomp
-  break if valid_number?(divisor)
-  divisor == '0' ? (puts "You can't divide by zero") : (puts "Integers only.")
-  puts "Integers only."
+  if divisor == '0'
+    puts "Invalid input. You can't divide by zero."
+  else
+    break if valid_number?(divisor)
+    puts "Invalid input. Integers only."
+  end
 end
-dividend = dividend.to_i
-divisor = divisor.to_i
-if divisor == 0
-  puts "You can't divide by zero."
-else 
-  puts "#{dividend} / #{divisor} is #{dividend / divisor}"
-end
+puts "#{dividend} / #{divisor} is #{dividend.to_i / divisor.to_i}"
+
