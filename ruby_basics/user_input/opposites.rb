c585a31fe55057595f6ea475292c9c381ac73e11
seanchavez <realseanchavez@gmail.com>
2021-04-21 22:06:01 -0700

    complete user input

--- ruby_basics/user_input/opposites.rb
+++ ruby_basics/user_input/opposites.rb
@@ -15,24 +15,7 @@ int_1 = int_2 = nil
 loop do
   int_1 = get_input
   int_2 = get_input
-  # int_str_1 = nil
-  # loop do
-  #   puts "Please enter a positive or negative integer:"
-  #   int_str_1 = gets.chomp
-  #   break if valid_number?(int_str_1)
-  #   puts "Invalid input. Only non-zero integers are allowed."  
-  # end
-  # int_str_2 = nil
-  # loop do
-  #   puts "Please enter a positive or negative integer:"
-  #   int_str_2 = gets.chomp
-  #   break if valid_number?(int_str_2)
-  #   puts "Invalid input. Only non-zero integers are allowed." 
-  # end
-  # int_1 = int_str_1.to_i
-  # int_2 = int_str_2.to_i
-break if !(int_1.positive? && int_2.positive? ||  
-           int_1.negative? && int_2.negative?)
+break if int_1 * int_2 < 0
   puts "Sorry. One integer must be positive, one must be negative."
   puts "Please start over."
 end
