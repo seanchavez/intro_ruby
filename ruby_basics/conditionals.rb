e10550be3f0cc6b55acdbb4bb53422e93fed917e
seanchavez <realseanchavez@gmail.com>
2021-04-24 16:30:36 -0700

    strings

--- ruby_basics/conditionals.rb
+++ ruby_basics/conditionals.rb
@@ -36,10 +36,21 @@ end
 stoplight = ['green', 'yellow', 'red'].sample
 
 case stoplight
-when 'green' then puts 'Go!'
-  
+when 'green' then puts 'Go!' 
 when 'yellow' then puts 'Slow down!'
-  
-else puts 'Stop!'
-  
+else puts 'Stop!' 
 end
+
+greeting = 'Hello!'
+greeting.replace('Goodbye!')
+puts greeting
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+puts alphabet.split('')
+
+words = 'car human elephant airplane'
+words.split.each {|word| puts word + 's'}
+
+colors = 'blue pink yellow orange'
+puts colors.include?('yellow') 
+puts colors.include?('false')
