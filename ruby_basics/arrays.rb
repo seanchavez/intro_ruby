dcf352dd2d52896e8a5d207a03405a1d2f709766
seanchavez <realseanchavez@gmail.com>
2021-04-24 23:41:35 -0700

    arrays

--- ruby_basics/arrays.rb
+++ ruby_basics/arrays.rb
@@ -12,3 +12,14 @@ my_pets = pets[2..3]
 my_pets.pop
 my_pets << pets[1]
 puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+
+colors = ['red', 'yellow', 'purple', 'green']
+colors.each {|color| puts "I'm the color #{color}!"}
+
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = numbers.map {|num| num * 2}
+p doubled_numbers
+
+numbers = [5, 9, 21, 26, 39]
+divisible_by_three = numbers.select {|num| num % 3 == 0}
+p divisible_by_three
