9d14742e2f0509b79c4d2b5999e07c853980031b
seanchavez <realseanchavez@gmail.com>
2021-04-26 10:23:19 -0700

    complete hashes

--- ruby_basics/hashes.rb
+++ ruby_basics/hashes.rb
@@ -11,3 +11,37 @@ numbers = {
   low:    10
 }
 numbers.each {|k, v| puts "#{k} => #{v}"}
+
+half_numbers = numbers.map {|_, num| num / 2}
+p half_numbers
+
+low_numbers = numbers.select {|_, num| num < 25}
+p low_numbers
+p numbers
+
+low_numbers = numbers.select! {|_, num| num < 25}
+p low_numbers
+p numbers
+
+vehicles = {
+  car: {
+    type: 'sedan',
+    color: 'blue',
+    year: 2003,
+  },
+  truck: {
+    type: 'pickup',
+    color: 'red',
+    year: 1998,
+  }
+}
+p vehicles
+
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+p car
+car_arr = [[:type, 'sedan'],[:color, 'blue'],[:year, '2003']]
+p car_arr
