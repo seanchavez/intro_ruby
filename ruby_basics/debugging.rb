562f12f7be13aa89528754d44f601dcc523ea5bf
seanchavez <realseanchavez@gmail.com>
2021-04-26 13:31:10 -0700

    debugging

--- ruby_basics/debugging.rb
+++ ruby_basics/debugging.rb
@@ -41,3 +41,75 @@ even_numbers = numbers.select do |n|
 end
 
 p even_numbers # expected output: [2, 6, 8]
+
+def get_quote(person)
+  if person == 'Yoda'
+   return 'Do. Or do not. There is no try.'
+  end
+
+  if person == 'Confucius'
+    return 'I hear and I forget. I see and I remember. I do and I understand.'
+  end
+
+  if person == 'Einstein'
+    return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
+
+puts 'Confucius says:'
+puts '"' + get_quote('Confucius') + '"'
+
+# Financially, you started the year with a clean slate.
+
+balance = 0
+
+# Here's what you earned and spent during the first three months.
+
+january = {
+  income: [ 1200, 75 ],
+  expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
+}
+
+february = {
+  income: [ 1200 ],
+  expenses: [ 650, 140, 320, 46.7, 122.5 ]
+}
+
+march = {
+  income: [ 1200, 10, 75 ],
+  expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
+}
+
+# Let's see how much you've got now...
+
+def calculate_balance(month)
+  plus  = month[:income].sum
+  minus = month[:expenses].sum
+
+  plus - minus
+end
+
+[january, february, march].each do |month|
+  balance += calculate_balance(month)
+end
+
+puts balance
+
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  break if !colors[i] || !things[i]
+
+    if i == 0
+      puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+    else
+      puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+    end
+
+  i += 1
+end
