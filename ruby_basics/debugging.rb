4dd3bf00791220de2c87b5871aeaf75da7c8cc76
seanchavez <realseanchavez@gmail.com>
2021-04-26 14:13:26 -0700

    complete debugging

--- ruby_basics/debugging.rb
+++ ruby_basics/debugging.rb
@@ -113,3 +113,37 @@ loop do
 
   i += 1
 end
+
+def digit_product(str_num)
+  digits = str_num.chars.map { |n| n.to_i }
+  product = 1
+
+  digits.each do |digit|
+    product *= digit
+  end
+
+  product
+end
+
+p digit_product('12345')
+
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then the player picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength:  20 },
+  thief:   { dexterity: 20 },
+  scout:   { stamina:   20 },
+  mage:    { charisma:  20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase
+
+player.merge!(character_classes[input.to_sym])
+
+puts 'Your character stats:'
+puts player
