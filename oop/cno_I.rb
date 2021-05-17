d8e5176a18157c23cdb3677d2139f8b7ca67fca3
seanchavez <realseanchavez@gmail.com>
2021-05-17 10:20:51 -0700

    exercise 3

--- oop/cno_I.rb
+++ oop/cno_I.rb
@@ -20,4 +20,16 @@ class MyCar
   def off
     @speed = 0
   end
+
+  def spray_paint(color)
+    self.color = color
+    puts "#{self} is now painted #{color}"
+  end
 end
+
+
+
+turd = MyCar.new(2016, 'blue', 'corolla')
+puts turd.color
+turd.spray_paint('grey')
+puts turd.color
