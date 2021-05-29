9bb3a665ee297e523d3abd3671a977100984efa2
seanchavez <realseanchavez@gmail.com>
2021-05-29 08:06:00 -0700

    is_valid_email

--- aa/intro.rb
+++ aa/intro.rb
@@ -6,3 +6,22 @@ def is_valid_name(str)
   end
   true
 end
+
+puts is_valid_name("Kush Patel")       # => true
+puts is_valid_name("Daniel")           # => false
+puts is_valid_name("Robert Downey Jr") # => true
+puts is_valid_name("ROBERT DOWNEY JR") # => false
+
+def is_valid_email(str)
+  parts = str.split("@")
+  return false if parts.size != 2 || parts[1].count(".") != 1
+  parts[0].each_char {|char| return false if !("a".."z").include?(char)}
+  true
+end
+
+puts is_valid_email("abc@xy.z")         # => true
+puts is_valid_email("jdoe@gmail.com")   # => true
+puts is_valid_email("jdoe@g@mail.com")  # => false
+puts is_valid_email("jdoe42@gmail.com") # => false
+puts is_valid_email("jdoegmail.com")    # => false
+puts is_valid_email("az@email")         # => false
