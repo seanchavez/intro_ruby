010f0cf16405daa64eb5d9c12221d04967dcf897
seanchavez <realseanchavez@gmail.com>
2021-05-30 17:44:15 -0700

    element_replace

--- aa/intro.rb
+++ aa/intro.rb
@@ -160,3 +160,17 @@ end
 
 print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
 puts
+
+def element_replace(arr, hash)
+  arr.map {|el| hash[el] ? hash[el] : el}
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
